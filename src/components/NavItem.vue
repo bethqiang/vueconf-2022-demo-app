9be9747d26b67d90cf802757f2d438fc3d0d8b74
Beth Qiang <beth.qiang@lob.com>
2022-05-29 16:53:32 -0600

    fix navitem losing active /images/id

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,27 +1,34 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
+import { computed } from 'vue';
 
-defineProps({
+const route = useRoute();
+
+const props = defineProps({
   to: { type: String, required: true },
   iconSrc: { type: String, required: true },
   iconAltText: { type: String, required: true },
   title: { type: String, required: true }
 });
+
+const active = computed(() => `/${route.path.split('/')[1]}` === props.to);
 </script>
 
 <template>
   <li class="list-none">
     <RouterLink
-      :to="to"
-      class="py-4 px-6 flex items-center"
-      active-class="font-medium underline bg-white-300"
+      :to="props.to"
+      :class="[
+        'py-4 px-6 flex items-center',
+        { 'font-medium underline bg-white-300': active }
+      ]"
     >
       <img
-        :src="iconSrc"
-        :alt="iconAltText"
+        :src="props.iconSrc"
+        :alt="props.iconAltText"
         class="w-6 align-bottom"
       >
-      <span class="pl-4">{{ title }}</span>
+      <span class="pl-4">{{ props.title }}</span>
     </RouterLink>
   </li>
 </template>
