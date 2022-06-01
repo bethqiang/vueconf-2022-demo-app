a937aefa252a5ea8aa74232bc49edb974e6c793a
Beth Qiang <beth.qiang@lob.com>
2022-06-01 10:38:05 -0600

    add info to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>Home Page</h1>
+  <h1>Home</h1>
+  <div class="mt-8">
+    <p class="mb-4">
+      This is a demo app created for my 2022 <NewLink to="https://vueconf.us/">VueConf US</NewLink> conference talk, "Unit Testing Vue Apps: Tips, Tricks, and Best Practices".</p>
+    <p class="mb-4">It is meant to represent a small subset of a real-life application, and was modeled after a portion of Lob's customer-facing Dashboard.</p>
+    <p class="mb-4">
+      The GitHub repo with unit tests can be found at <NewLink to="https://github.com/bethqiang/vueconf-2022-demo-app">https://github.com/bethqiang/vueconf-2022-demo-app</NewLink>.
+    </p>
+  </div>
 </template>
